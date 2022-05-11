f4822839f734a4566f711ceb42d78628bdf7610b
Nova <nova940116@gmail.com>
2022-05-11 17:10:11 +0900

    add vercel deploy config

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import preprocess from 'svelte-preprocess'
 import { mdsvex } from 'mdsvex'
+import vercel from '@sveltejs/adapter-vercel'
 
 /** @type {import('@sveltejs/kit').Config} */
 
@@ -15,7 +16,23 @@ const config = {
 			},	
 		})
 	],
-	kit: {},
+  kit: {
+    // default options are shown
+    adapter: vercel({
+      // if true, will deploy the app using edge functions
+      // (https://vercel.com/docs/concepts/functions/edge-functions)
+      // rather than serverless functions
+      edge: false,
+
+      // an array of dependencies that esbuild should treat
+      // as external when bundling functions
+      external: [],
+
+      // if true, will split your app into multiple functions
+      // instead of creating a single one for the entire app
+      split: false
+    })
+  },
 	extensions: ['.svelte', '.md'],
 }
 
